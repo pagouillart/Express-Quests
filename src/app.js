dc7f0f6edfb4763ed997c01b2231bec8a1ebd242
Pierre-Adrien Gouillart <pagouillart@hotmail.fr>
2023-11-28 11:46:45 +0100

    challenge quest2 done

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const movieControllers = require("./controllers/movieControllers");
 const usersController = require("./controllers/usersController");
 
@@ -10,5 +12,8 @@ app.get("/api/movies/:id", movieControllers.getMovieById);
 
 app.get("/api/users", usersController.getUsers);
 app.get("/api/users/:id", usersController.getUsersById);
+
+app.post("/api/movies", movieControllers.postMovie);
+app.post("/api/users", usersController.postUser);
 
 module.exports = app;
